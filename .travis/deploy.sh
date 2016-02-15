722276aa6298ee9d0b44b1f8a339e2abb64bf26c
Luca Iannario <lucaiannario@gmail.com>
2016-02-15 20:50:12 +0000

    targetCI

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,13 +4,10 @@ set -ev
 env
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-	echo "test"
-	ls -al .travis
-
-	eval "$(ssh-agent -s)" #start the ssh agent
+	eval $(ssh-agent -s) #start the ssh agent
 	chmod 600 .travis/id_rsa # this key should have push access
 	ssh-add .travis/id_rsa
-	git remote add targetCI git@github.com:liannario/targetCI.git
+	git remote add targetCI https://liannario:9875dc64abbd6f01232839cad47b98ff6cbeb84c:github.com/liannario/targetCI.git
 	git push targetCI "$TRAVIS_BRANCH"
 	rm .travis/id_rsa
 fi
